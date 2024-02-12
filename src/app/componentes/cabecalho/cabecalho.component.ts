93420048f1b0cd6d3721d65e83a99945e32ad52d
Gabriel-S-Santana <gabrielsouzasantana2@gmail.com>
2024-02-12 16:26:39 -0300

    feat - exclusão de pensamentos

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabecalhoComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
--- src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { PensamentoService } from '../pensamento.service';
+import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./excluir-pensamento.component.css']
 })
 export class ExcluirPensamentoComponent implements OnInit {
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
 
-  constructor() { }
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscaPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento;
+    })
+  }
+
+  excluirPensamento() {
+    if(this.pensamento.id) {
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
   }
 
 }
